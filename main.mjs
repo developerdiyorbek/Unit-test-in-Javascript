d5156cb74ce16af4985fff7f26040639694189ea
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-08-12 02:39:21 +0500

    testing arrays

--- main.mjs
+++ main.mjs
@@ -1,3 +1,9 @@
+export const users = ["Diyorbek"];
+
 export function sum(n1, n2) {
   return n1 + n2;
+}
+
+export function addNewItem(item) {
+  users.push(item);
 }
--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,7 +1,14 @@
-import { sum } from "../main.mjs";
+import { addNewItem, sum, users } from "../main.mjs";
 
 describe("Main js", () => {
   it("should add two numbers", () => {
     expect(sum(10, 1)).toBe(11);
+  });
+
+  it("should add new item in users array", () => {
+    const usersLength = users.length;
+    addNewItem("Dilshodbek");
+    expect(users).toContain("Dilshodbek");
+    expect(users.length).toEqual(usersLength + 1);
   });
 });
