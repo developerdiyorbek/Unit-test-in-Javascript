ba08b39b0bc58a6f739fe723b72b7ce91b87540a
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-08-12 19:50:30 +0500

    test with Jasmine

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -3,6 +3,8 @@ import { addNewItem, sum, users } from "../main.mjs";
 describe("Main js", () => {
   it("should add two numbers", () => {
     expect(sum(10, 1)).toBe(11);
+    expect(sum(10, 1)).toBeTruthy();
+    expect(sum(10, 1)).toBeGreaterThan(10);
   });
 
   it("should add new item in users array", () => {
